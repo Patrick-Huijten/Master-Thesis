4b51111c5f36c4559477982cb3bf188117998b36
Patrick Huijten <p.j.huijten@student.tue.nl>
2025-05-01 17:02:04 +0200

    Added file selection button

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from dash import Dash, html
+import dash
+from dash import html, dcc, Input, Output, State, Dash, ctx
 import dash_bootstrap_components as dbc
 
-external_stylesheets = [dbc.themes.SLATE]
+external_stylesheets = [dbc.themes.CYBORG]
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = "Mental Health Dataset Analysis."
+app.title = "MannaertsAppels Dashboard"
 
-app.layout = html.Div([
-    html.H1("Hello World")
+app.layout = dbc.Container([
+    #Construct the title and subtitles
+    dbc.Row([
+        html.Div('MannaertsAppels Dashboard', style={
+            'fontSize': '36px', 
+            'fontWeight': 'bold', 
+            'textAlign': 'center', 
+            'marginBottom': '20px'
+        })
+    ]),
+
+    dbc.Row([
+        dcc.Upload(
+            id='upload-data',
+            children=dbc.Button("Select File", color="primary"),
+            multiple=False,
+            style={'display': 'inline-block'}
+        )
+    ])
 ])
 
 if __name__ == '__main__':
