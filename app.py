23fee855c822eb91858d96872858795a1c284ddd
Patrick Huijten <p.j.huijten@student.tue.nl>
2025-05-01 11:40:14 +0200

    First Dash components

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-print("Hello World")
+from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-# Run the app
+external_stylesheets = [dbc.themes.SLATE]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Mental Health Dataset Analysis."
+
+app.layout = html.Div([
+    html.H1("Hello World")
+])
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
