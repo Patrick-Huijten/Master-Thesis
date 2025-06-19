35eabef923540755e67280a4f573d926421f68b4
Patrick Huijten <p.j.huijten@student.tue.nl>
2025-06-19 14:26:30 +0200

    Updated requirements file

--- test2.py
+++ test2.py
@@ -1,2 +1,6 @@
-import dash_annotator
-print(dir(dash_annotator))
+# import dash_annotator
+# import dash_annotator
+from dash_annotator import TextAnnotator
+print('df')
+print('hello')
+# print(dir(dash_annotator))
